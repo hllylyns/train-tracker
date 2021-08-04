b18962328c9cbaf428a94ed6e1577a739b81bce6
Holly Lyons <holly.lyons@podium.com>
2021-08-03 23:56:04 -0600

    create elixir app

---
+++ train_tracker/lib/train_tracker.ex
@@ -0,0 +1,18 @@
+defmodule TrainTracker do
+  @moduledoc """
+  Documentation for TrainTracker.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TrainTracker.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ train_tracker/test/train_tracker_test.exs
@@ -0,0 +1,8 @@
+defmodule TrainTrackerTest do
+  use ExUnit.Case
+  doctest TrainTracker
+
+  test "greets the world" do
+    assert TrainTracker.hello() == :world
+  end
+end
